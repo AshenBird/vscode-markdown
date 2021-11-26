a25e804d5dde39c5ac09dae6a08ed1bf71b5e363
liangzizhou <liangzizhou@youcku.com>
2021-11-26 17:53:04 +0800

    up

--- scripts/build.js
+++ scripts/build.js
@@ -1,31 +1,13 @@
 const execa = require("execa");
 const fs = require("fs-extra");
-const path = require("path");
 const vite = require("vite");
-
-const getPath = (p) => path.resolve(__dirname, "../", p);
+const { createClientBuildConfig, getPath } = require("./utils");
 
 fs.ensureDir(getPath("out"));
-
 const build = async () => {
-  const vditorBuilder = vite.build({
-    root:getPath("src/client/vditor/"),
-    // base:"mcswift://",
-    base:"./",
-    build:{
-      outDir:getPath("out/client/vditor/"),
-      emptyOutDir:true,
-    }
-  });
-  const milkdownBuilder = vite.build({
-    root:getPath("src/client/milkdown/"),
-    // base:"mcswift://",
-    base:"./",
-    build:{
-      outDir:getPath("out/client/milkdown/"),
-      emptyOutDir:true,
-    }
-  });
+
+  const vditorWatcher = vite.build(createClientBuildConfig("vditor"));
+  const milkdownWatcher = vite.build(createClientBuildConfig("milkdown"));
 
   const hostBuilder = execa("npm", ["run", "build:host"]);
   hostBuilder.stdout.pipe(process.stdout);
--- scripts/watch.js
+++ scripts/watch.js
@@ -1,38 +1,18 @@
 const execa = require("execa");
 const fs = require("fs-extra");
-const path = require("path");
 const vite = require("vite");
-
-const getPath = (p) => path.resolve(__dirname, "../", p);
+const { createClientBuildConfig, getPath } = require("./utils");
 
 fs.ensureDir(getPath("out"));
 
 const watch = async () => {
-  const vditorWatcher = vite.build({
-    root:getPath("src/client/vditor/"),
-    // base:"mcswift://",
-    base:"./",
-    build:{
-      outDir:getPath("out/client/vditor/"),
-      emptyOutDir:true,
-      watch:{}
-    }
-  });
-  const milkdownWatcher = vite.build({
-    root:getPath("src/client/milkdown/"),
-    // base:"mcswift://",
-    base:"./",
-    build:{
-      outDir:getPath("out/client/milkdown/"),
-      emptyOutDir:true,
-      watch:{}
-    }
-  });
+  const vditorWatcher = vite.build(createClientBuildConfig("vditor",{}));
+  const milkdownWatcher = vite.build(createClientBuildConfig("milkdown",{}));
 
   const hostWatcher = execa("npm", ["run", "watch:host"]);
   hostWatcher.stdout.pipe(process.stdout);
 
-  // return { vditorWatcher, hostWatcher };
+  return { vditorWatcher, milkdownWatcher, hostWatcher };
 };
 
 watch();
