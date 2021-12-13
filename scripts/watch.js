29e2c78405c9aa35bbffb7206332cf7cc85d08d6
liangzizhou <liangzizhou@youcku.com>
2021-12-13 11:16:41 +0800

    v0.1.10

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,15 +1,16 @@
 const execa = require("execa");
 const fs = require("fs-extra");
 const vite = require("vite");
+const viteSSG  =require("vite-ssg");
 const { createClientBuildConfig, getPath } = require("./utils");
 
 fs.ensureDir(getPath("out"));
 
 const watch = async () => {
-  // const vditorWatcher = vite.build(createClientBuildConfig("vditor",{}));
-  const milkdownWatcher = vite.build(createClientBuildConfig("milkdown",{}));
-
-  const hostWatcher = execa("npm", ["run", "watch:host"]);
+  // const milkdownWatcher = vite.build(createClientBuildConfig("milkdown",{}));
+  const clientWatcher = execa("pnpm", ["run", "watch:client"]);
+  clientWatcher.stdout.pipe(process.stdout);
+  const hostWatcher = execa("pnpm", ["run", "watch:host"]);
   hostWatcher.stdout.pipe(process.stdout);
 };
 
--- src/client/milkdown/vite.config.ts
+++ src/client/milkdown/vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from "vite";
-import vue from '@vitejs/plugin-vue';
+import vue from "@vitejs/plugin-vue";
 import jsx from "@vitejs/plugin-vue-jsx";
+import path from "path";
 
+const getPath = (p: string) => path.resolve(__dirname, "../../../", p);
 export default defineConfig({
-  plugins:[
-    vue(),
-    jsx()
-  ]
+  root: getPath(`src/client/milkdown/`),
+  base: "./",
+  // publicDir,
+  build: {
+    outDir: getPath(`out/client/milkdown/`),
+    emptyOutDir: true,
+    watch:process.env.MODE==="watch"?{}:undefined,
+  },
+  plugins: [vue(), jsx()],
+  ssgOptions: {
+    mock: true
+  },
+  resolve: {
+    // 别名
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
+  },
 });
